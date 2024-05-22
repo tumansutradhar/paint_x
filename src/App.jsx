4c06eb45ee35d50fad61e31d0af912792fdd6eb7
Tuman Sutradhar <officialbishalsutradhar@gmail.com>
2024-05-22 17:47:05 +0530

    add src folder

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+import React, { useEffect, useRef, useState } from "react";
+import Menu from "./components/Menu";
+import "./App.css";
+
+function App() {
+	const canvasRef = useRef(null);
+	const contextRef = useRef(null);
+	const [isDrawing, setIsDrawing] = useState(false);
+	const [lineWidth, setLineWidth] = useState(10);
+	const [lineColor, setLineColor] = useState("white");
+	const [lineOpacity, setLineOpacity] = useState(0.1);
+
+	useEffect(() => {
+		const canvas = canvasRef.current;
+		canvas.width = window.innerWidth;
+		canvas.height = window.innerHeight;
+		const context = canvas.getContext("2d");
+		context.lineCap = "round";
+		context.lineJoin = "round";
+		contextRef.current = context;
+	}, []);
+
+	useEffect(() => {
+		if (contextRef.current) {
+			contextRef.current.lineWidth = lineWidth;
+		}
+	}, [lineWidth]);
+
+	useEffect(() => {
+		if (contextRef.current) {
+			contextRef.current.strokeStyle = lineColor;
+		}
+	}, [lineColor]);
+
+	useEffect(() => {
+		if (contextRef.current) {
+			contextRef.current.globalAlpha = lineOpacity;
+		}
+	}, [lineOpacity]);
+
+	const startDrawing = (e) => {
+		contextRef.current.beginPath();
+		contextRef.current.moveTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
+		setIsDrawing(true);
+	};
+
+	const endDrawing = () => {
+		contextRef.current.closePath();
+		setIsDrawing(false);
+	};
+
+	const draw = (e) => {
+		if (!isDrawing) {
+			return;
+		}
+		contextRef.current.lineTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
+		contextRef.current.stroke();
+	};
+
+	return (
+		<div className="w-full h-screen flex flex-col justify-center items-center relative bg-black">
+			<Menu setLineColor={setLineColor} setLineWidth={setLineWidth} setLineOpacity={setLineOpacity} />
+			<canvas onMouseDown={startDrawing} onMouseUp={endDrawing} onMouseMove={draw} ref={canvasRef} style={{ width: '100%', height: '100vh' }} />
+		</div>
+	);
+}
+
+export default App;
---
+++ src/components/Menu.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Menu = ({ setLineColor, setLineWidth, setLineOpacity }) => {
+    return (
+        <div className="flex items-center flex-col absolute top-3 p-5 bg-zinc-800 text-white gap-1 border-none rounded-md sm:flex-row">
+            <label htmlFor="brushColor">Brush Color:</label>
+            <input className="bg-transparent" id="brushColor" type="color" onChange={(e) => { setLineColor(e.target.value); }} />
+            <label htmlFor="brushWidth">Brush Width:</label>
+            <input id="brushWidth" type="range" min="1" max="10" onChange={(e) => { setLineWidth(e.target.value); }} />
+            <label htmlFor="brushOpacity">Brush Opacity:</label>
+            <input id="brushOpacity" type="range" min="1" max="100" onChange={(e) => { setLineOpacity(e.target.value / 100); }} />
+        </div>
+    );
+};
+
+export default Menu;
